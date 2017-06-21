26fb61951b77ea919d95ec4a4421b61c7b458e5a
a-nechiporenko <toxa7991@gmail.com>
2017-06-21 19:55:35 +0300

    ADD TO FOLDERS

---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+#include"BattleShips.h"
+#include"Enum.h"
+
+
+int main()
+{
+	BattleShips bs;
+
+	while (!bs.gameOver()) {
+		bs.gameUpdate();
+	}
+	return 0;
+}
+
+//complete in soon...
+
+//class Board{
+//};
+//
+//class Menu{
+//};
