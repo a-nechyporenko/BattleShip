a19f7bc67218ef20253f0d4ebc2a268eabda2866
a-nechiporenko <toxa7991@gmail.com>
2017-06-22 06:20:56 +0300

    User class

--- source/HumanPlayer.h
+++ source/HumanPlayer.h
@@ -7,9 +7,13 @@
 class HumanPlayer :public Player, public Fild {
 public:
 	HumanPlayer();
+	char numbers[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	char lete[10] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+	int message;	// The variable stores the message codes (0 - everything is fine, 1 - the user pressed the wrong key, 2 - the game is over)
+	int input(int&, int&, int massage);					//User input
 	~HumanPlayer();
 private:
-	int input(int&, int&);					//User input
+	
 };
 
 #endif
